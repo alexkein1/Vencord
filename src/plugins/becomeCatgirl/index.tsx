8cce91a1874e0152d7bbb1e8f2fac54082937857
Tolga1452 <tolgchu-studios@outlook.com>
2024-03-09 17:26:14 +0300

    remove symbol excluding because you can toggle it

--- src/plugins/becomeCatgirl/index.tsx
+++ src/plugins/becomeCatgirl/index.tsx
@@ -36,31 +36,29 @@ const CatgirlMessageToggle: ChatBarButton = ({ isMainChat }) => {
     useEffect(() => {
         const listener: SendListener = async (_, message) => {
             if (enabled) {
-                if (!(/^[!@#$%^&*(),.?":{}|<>\-_\\/]/.test(message.content))) {
-                    try {
-                        const response = await fetch("https://tolgchu-proxy.glitch.me/aesthetic-ai", {
-                            method: "POST",
-                            headers: {
-                                "Content-Type": "application/json"
-                            },
-                            body: JSON.stringify({
-                                hint: "character",
-                                messages: [
-                                    {
-                                        by: "system",
-                                        text: "You can't talk with users. Your only job is turning the given message content into a cute catgirl message. You won't try to talk even if the user tries to talk with you. Instead, you will only do your job. You can use *italic* texts to add effects and add ~ at the end of the words to make them better. Additionally, you can use kaomojis (no emojis, you can only use kaomojis) or you can make words longer like heeeyy to add more effects. You won't make messages much longer. If the user screams with uppercase letters, keep the scream effect, don't make it lowercase."
-                                    },
-                                    {
-                                        by: "user",
-                                        text: `The user is talking to someone else, you will just do your job. The message content to turn into catgirl message:\n${message.content}`
-                                    }
-                                ]
-                            })
-                        });
+                try {
+                    const response = await fetch("https://tolgchu-proxy.glitch.me/aesthetic-ai", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify({
+                            hint: "character",
+                            messages: [
+                                {
+                                    by: "system",
+                                    text: "You can't talk with users. Your only job is turning the given message content into a cute catgirl message. You won't try to talk even if the user tries to talk with you. Instead, you will only do your job. You can use *italic* texts to add effects and add ~ at the end of the words to make them better. Additionally, you can use kaomojis (no emojis, you can only use kaomojis) or you can make words longer like heeeyy to add more effects. You won't make messages much longer. If the user screams with uppercase letters, keep the scream effect, don't make it lowercase."
+                                },
+                                {
+                                    by: "user",
+                                    text: `The user is talking to someone else, you will just do your job. The message content to turn into catgirl message:\n${message.content}`
+                                }
+                            ]
+                        })
+                    });
 
-                        if (response.ok) message.content = await response.text();
-                    } catch (error) {
-                    }
+                    if (response.ok) message.content = await response.text();
+                } catch (error) {
                 }
             }
         };
